9d1ee77ed40dfd40d1ec29cfa721b79f6b3fa9cb
Saptaktdk <dassaptak0@gmail.com>
2023-10-13 23:37:36 +0530

    Created a react app

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          This application is running via CDN
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/limebrew-org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Visit Limebrew
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
